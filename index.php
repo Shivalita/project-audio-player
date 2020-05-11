cc620d78c2e149e25e63bf221bb788cc286906cf
Audrene <audrene.coatmeur@gmail.com>
2020-05-11 19:45:52 +0200

    responsive except transision

--- index.php
+++ index.php
@@ -10,13 +10,33 @@ include_once './partials/start-session.php';
     ?>
     <title>Index</title>
 </head>
-<body>
+    <body>
+
+        <div class="container-fluid" id="container">
+
+            <div class="row" id="row1">
+                <span><i class="fas fa-chevron-circle-right"></i></span>
+                <aside class="col-12 col-md-2" id="side">
+                    <p>Here's our aside, which contains our user's menu and our search bar</p>
+                </aside>
+                <div class="col-12 col-md-10" id="main">
+                    <p>Here's our main div, which display the content of our four tabs</p>
+                </div>
+            </div>
+            
+            <div class="row" id="row2">
+                <section class="col-12" id="player">
+                    <p>Here's our player section, which contains all you need to listen to music</p>
+                </section>
+            </div>
+
+        </div>
   
     
 
-    <?php
-    include_once './partials/script.php';
-    ?>
+        <?php
+        include_once './partials/script.php';
+        ?>
 
-</body>
+    </body>
 </html>
