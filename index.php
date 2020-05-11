69a17c2d8556d1f375803c8ffc10718849a2b709
Audrene <audrene.coatmeur@gmail.com>
2020-05-11 10:15:01 +0200

    all files are linked

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include_once './partials/start-session.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include_once './partials/head.php';
+    ?>
+    <title>Index</title>
+</head>
+<body>
+  
+    
+
+    <?php
+    include_once './partials/script.php';
+    ?>
+
+</body>
+</html>
